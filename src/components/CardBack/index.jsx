32238d60b3973f57922e0a845d4d791b883da2cb
florenciafer <fernandez.gestoria@hotmail.com>
2020-10-01 14:42:04 -0300

    se agrego style y media querie

--- src/components/CardBack/index.jsx
+++ src/components/CardBack/index.jsx
@@ -5,24 +5,27 @@ import { GiSandsOfTime } from "react-icons/gi";
 const CardBack = ({ nombre, puntuacion, dificultad, tiempo }) => {
     return (
         <div className="CardBack">
-            <h1 className="Titulo-CardBack">{nombre}</h1>
-            <p className="Subtitulo-CardBack">
-                Puntuación:
-        <span>
-                    <Puntaje icono="heart" puntuacion={puntuacion}></Puntaje>
-                </span>
-            </p>
-            <p className="Subtitulo-CardBack">
-                Dificultad:
-        <span>
-                    <Puntaje icono="star" puntuacion={dificultad}></Puntaje>
-                </span>
-            </p>
+            <div className="CardBack-container">
+                <h1 className="Titulo-CardBack">{nombre}</h1>
+                <p className="Subtitulo-CardBack">
+                    Puntuación:
+        <span className="puntaje">
+                        <Puntaje icono="heart" puntuacion={puntuacion}></Puntaje>
+                    </span>
+                </p>
+                <p className="Subtitulo-CardBack">
+                    Dificultad:
+        <span className="dificultad">
+                        <Puntaje icono="star" puntuacion={dificultad}></Puntaje>
+                    </span>
+                </p>
 
-            <p className="Subtitulo-CardBack">
-                Tiempo: {tiempo}
-                <GiSandsOfTime />
-            </p>
+                <p className="Subtitulo-CardBack">
+                    Tiempo: {tiempo}
+                    <GiSandsOfTime />
+                </p>
+            </div>
+
         </div>
     );
 };
